c6ab932a4002501640b878da76f2eea8da861add
Adam Herro <adam.herro@comcast.net>
2023-03-29 21:48:14 -0400

    Adding components for task area and adding a delete button to the task card.

--- src/components/TaskContainer.jsx
+++ src/components/TaskContainer.jsx
@@ -1,9 +1,10 @@
+import Tasks from "./Tasks"
+
 const TaskContainer = () => {
   return (
-    <div className='item-card card justify-self-center w-max mt-16 bg-zinc-800'>
-        <div className="card-title text-white p-5 pl-10">Item 1</div>
-        <div className="card-body text-white p-5 pt-1 pl-10">Do 5 React and Node/Express tutorials.</div>
-    </div>
+    <>
+        <Tasks />
+    </>
   )
 }
 
